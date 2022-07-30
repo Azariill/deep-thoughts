75d5d3d3264914759814fdf6af8ea3e88e4118ab
Azarill <heierms@gmail.com>
2022-07-30 00:53:49 -0500

    added login and signup functionality using webtokens

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,29 @@ import Signup from './pages/Signup';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import { setContext } from '@apollo/client/link/context';
 
-// ESTABLISH A LINK TO GRAPHQL SERVER
+// ESTABLISH A LINK TO GRAPHQL SERVER -- setup a proxy server in json file to filter to 
 const httpLink = createHttpLink({
   uri: '/graphql'
 });
 
 
+
+// authenication that grabs your token and sets it with the headers
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// middleware that connects apollo client
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
